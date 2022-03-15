b76e3e699d4e9fa988bee24942f644d45db62e10
bobding <bobding@msn.com>
2022-03-15 13:49:00 -0700

    circular rover ok

---
+++ build/modules/px4_command/CMakeFiles/circular_car_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/demo/Public/github_px4/some/devel/lib/px4_command/circular_car_node"
+  "/home/demo/Public/github_px4/some/devel/lib/px4_command/circular_car_node.pdb"
+  "CMakeFiles/circular_car_node.dir/src/Application/circular_car.cpp.o"
+  "CMakeFiles/circular_car_node.dir/src/Application/circular_car.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circular_car_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/modules/px4_command/CMakeFiles/circular_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/demo/Public/github_px4/some/devel/lib/px4_command/circular_node"
+  "/home/demo/Public/github_px4/some/devel/lib/px4_command/circular_node.pdb"
+  "CMakeFiles/circular_node.dir/src/Application/circular.cpp.o"
+  "CMakeFiles/circular_node.dir/src/Application/circular.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circular_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
